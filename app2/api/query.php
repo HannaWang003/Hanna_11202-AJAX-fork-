e55c25f2e3c3c49fae714891b163f7418a14773a
Hanna <hannawang003@gmail.com>
2023-12-22 14:48:47 +0800

    完成query

--- app2/api/query.php
+++ app2/api/query.php
@@ -1,5 +1,13 @@
 <?php
 //處理查詢資料的請求
-
-
-?>
+include_once "db.php";
+$Student = new DB('students');
+switch ($_GET['do']) {
+    case 'all':
+        header('Content-Type:application/json;charset=utf8');
+        echo json_encode($Student->all());
+        break;
+    case 'def':
+        dd($_GET);
+        break;
+}
