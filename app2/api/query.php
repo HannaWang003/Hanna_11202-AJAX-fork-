7e5f7823fcc9ef8e767c2161259930a20b3eaf15
HannaWang003 <hannawang003@gmail.com>
2023-12-23 13:07:43 +0800

    完成男生女生的篩選(function query(type,val)

--- app2/api/query.php
+++ app2/api/query.php
@@ -5,12 +5,15 @@ switch($_GET['do']){
     case 'all':
 header('Content-Type:applaction/json;charset:utf-8');
 echo json_encode($Student->all());
-        break;
-    case 'def':
-dd($_GET);
-        break;
+break;
+//     case 'def':
+    // dd($_GET);
+    //         break;
     case 'sex':
-dd($_GET);
+        // dd($_GET['val']);
+        header('Content-Type:applaction/json;charset:utf-8');
+        $users = $Student->q("select `name`,`uni_id`,`school_num`,`birthday` from `students` where substr(`uni_id`,2,1)='{$_GET['val']}'");
+        echo json_encode($users);
         break;
     case 'class':
 dd($_GET);
