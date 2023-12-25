7ab1c2e00f8cbfb104251058c10fe817e83c0de4
Hanna <hannawang003@gmail.com>
2023-12-25 11:57:20 +0800

    使用同一個form，並用id判斷新增或編輯

--- app2/api/user_form.php
+++ app2/api/user_form.php
@@ -3,27 +3,38 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h1 class="modal-title fs-5" id="studentFormLabel">
-                    新增 - 學生資料
+                    <?= (isset($_GET['id'])) ? '編輯學生' : '新增學生'; ?>
                 </h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <form action="./api/insert.php" method="post" class="row p-3">
+                    <?php
+
+                    if (isset($_GET['id'])) {
+                        include_once "db.php";
+                        $user = $Student->find($_GET['id']);
+                        extract($user);
+                    }
+
+                    ?>
                     <div class="mb-3 col-6">
                         <label for="name" class="form-label">姓名</label>
-                        <input type="text" class="form-control" name="name" id="name" />
+                        <input type="text" class="form-control" name="name" id="name" value="<?= $name??'' ?>" />
                     </div>
                     <div class="mb-3 col-6">
                         <label for="school_num" class="form-label">學號</label>
-                        <input type="text" class="form-control" name="school_num" id="school_num" />
+                        <input type="text" class="form-control" name="school_num" id="school_num"
+                            value="<?= $school_num??'' ?>" />
                     </div>
                     <div class="mb-3 col-6">
                         <label for="birthday" class="form-label">生日</label>
-                        <input type="date" class="form-control" id="birthday" id="birthday" />
+                        <input type="date" class="form-control" id="birthday" id="birthday"
+                            value="<?= $birthday??'' ?>" />
                     </div>
                     <div class="mb-3 col-6">
                         <label for="uni_id" class="form-label">身份證字號</label>
-                        <input type="text" class="form-control" name="uni_id" id="uni_id" />
+                        <input type="text" class="form-control" name="uni_id" id="uni_id" value="<?= $uni_id??'' ?>" />
                     </div>
                     <div class="mb-3 col-6">
                         <label for="addr" class="form-label">地址</label>
@@ -54,7 +65,7 @@
                             取消
                         </button>
                         <button type="submit" class="btn btn-primary">
-                            確認新增
+                            <?= (isset($_GET['id'])) ? '更新確認' : '確認新增'; ?>
                         </button>
                     </div>
                 </form>
